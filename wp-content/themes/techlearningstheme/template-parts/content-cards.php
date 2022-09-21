3b836e7189d251c9912bb5e0c65eb988334ac4cd
Zishan <zishanmohammed02@gmail.com>
2022-09-21 23:41:48 +0530

    added the ones with content

--- wp-content/themes/techlearningstheme/template-parts/content-cards.php
+++ wp-content/themes/techlearningstheme/template-parts/content-cards.php
@@ -31,7 +31,7 @@
   </a>
 </div>
 </div></a>
-<div class="col">
+<!--<div class="col">
 <a href="https://techlearnings.in/category/algorithm">
 <div class="card" style="width: 18rem;height : auto;">
   <img class="card-img-top" src="https://techlearnings.in/wp-content/uploads/2022/08/algorithm_full.jpeg" alt="Card image cap">
@@ -48,6 +48,16 @@
   <div class="card-body">
    <h5 class="card-title">Design Patterns</h5></a>
   </div>
+</div>
+</div>-->
+<div class="col">
+<a href="https://techlearnings.in/category/python">
+<div class="card" style="width: 18rem;height : auto;">
+  <img class="card-img-top" src="https://techlearnings.in/wp-content/uploads/2022/09/python.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Python</h5>
+  </div>
+  </a>
 </div>
 </div>
 </div>
