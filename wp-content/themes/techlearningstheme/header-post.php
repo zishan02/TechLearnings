694f3cdf651f8b95e3abf70891871046e5a5482d
zishan02 <zishanmohammed02@gmail.com>
2022-09-29 13:28:31 +0530

    Update header-post.php

--- wp-content/themes/techlearningstheme/header-post.php
+++ wp-content/themes/techlearningstheme/header-post.php
@@ -22,6 +22,9 @@
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&family=Roboto:ital,wght@0,100;0,300;1,100&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://techlearnings.in/wp-content/themes/techlearningstheme/custom.css">
 
 
